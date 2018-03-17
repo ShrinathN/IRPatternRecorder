0ed11c58d77f5f46e20886d2a8ed02dcf0418242
ShrinathN <shrinath.nimare@gmail.com>
2018-03-17 19:59:25 +0530

    Fixed all problems, dumps 400 samples reliably

--- main.c
+++ main.c
@@ -17,11 +17,11 @@
 #include </usr/avr/include/util/delay.h>
 #include </usr/avr/include/avr/eeprom.h>
 #include </usr/avr/include/avr/interrupt.h>
-#define SAMPLES 500
+#define SAMPLES 400
 
 typedef unsigned int WORD;
 typedef unsigned char BYTE;
-BYTE counter = 0, tempcounter = 0, memory[SAMPLES], temp;
+WORD memory[SAMPLES],temp, counter, tempcounter;
 
 //interupt setup
 void inter()
@@ -36,9 +36,9 @@ void uart_init()
 {
     UCSRB |= (1 << TXEN) | (1 << RXEN) | (1 << RXCIE); //enabling TX, RX and RX interrupt
     UCSRC |= (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1); //8 data bits
-    UBRRL = 0b00000100; //300 BAUD
-    UBRRH = 0b1101;
-//    UBRRL = 103; //setting baud rate to 9600, this can be changed for higher or lower
+//    UBRRL = 0b00000100; //300 BAUD
+//    UBRRH = 0b1101;
+    UBRRL = 103; //setting baud rate to 9600, this can be changed for higher or lower
 }
 
 //routine to send data through uart
@@ -51,8 +51,9 @@ void uart_send(BYTE data)
 //INT0 service routine
 ISR(INT0_vect)
 {
-    memory[counter++] = TCNT1L; //saving to memory, incrementing counter after
+    memory[counter++] = TCNT1; //saving to memory, incrementing counter after
     TCNT1 = 0; //reseting timer
+    memory[counter - 1] |= (~PIND & (1 << PD2))?(1 << 15):0;
     if(counter > SAMPLES) //if the sampling is done, turn on the status led, aka PB1
     {
 	GICR &= ~(1 << INT0); //we turn off the INT0 (this) interrupt too
@@ -69,19 +70,28 @@ ISR(USART_RXC_vect)
     uart_send('s');
     uart_send('t');
     uart_send('\n');
+    uart_send('\r'); //return carriage
     for(tempcounter = 0; tempcounter < SAMPLES;tempcounter++) //loop to print all the data out of uart. tempcounter is used so that even if not all SAMPLES are taken, the data can still be dumped
     {
 	temp = memory[tempcounter]; //setting temp as memory
+	if(temp & (1 << 15))
+	    uart_send('+');
+	else
+	    uart_send('-');
+	temp &= ~(1 << 15);
 	//binary format
 	/*
 	uart_send((temp >> 8) & 0xff);
 	uart_send(temp & 0xff);
 	*/
 	//ASCII format, sending the data as A
+	uart_send(48 + (temp / 10000) % 10);
+	uart_send(48 + (temp / 1000) % 10);
 	uart_send(48 + (temp / 100) % 10);
 	uart_send(48 + (temp / 10) % 10);
 	uart_send(48 + (temp % 10));
 	uart_send('\n'); //newline
+	uart_send('\r'); //return carriage
     }
     temp = UDR; //reading UDR because the datasheet states that in the ISR for RXC, UDR has to be read at least once to avoid an interrupt loop
 }
